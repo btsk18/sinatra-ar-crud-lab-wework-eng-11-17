6b9866d6a979da49fd2982a4026e7fe5dd2315a2
Brandon Leeds <brandonleeds@gmail.com>
2018-02-14 21:03:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    
+
   end
 
   get '/posts/new' do
@@ -18,5 +18,9 @@ class ApplicationController < Sinatra::Base
   post '/posts' do
     @post = Post.create(:name => params[:name], :content => params[:content])
     erb :show
+  end
+
+  get '/posts' do
+
   end
 end
